a4a68b9a65940837cf6011289524015f561a93ec
R0H-N <rohn.py@gmail.com>
2024-09-11 15:00:49 +0530

    made profile models

--- a_users/models.py
+++ a_users/models.py
@@ -8,4 +8,22 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='avatars/',null=True,blank=True)
     displayname = models.CharField(max_length=20,null=True,blank=True)
     info = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+    @property
+    def name(self):
+        if self.displayname:
+            name = self.displayname
+        else:
+            name = self.user.username
+        return name    
     
+    @property
+    def avatar(self):
+        try:
+            avatar = self.image.url
+        except:
+            avatar = static('images/avatar.svg')
+        return avatar
